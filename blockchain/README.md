da41632e7cb0bab145a5ceced07da987d180d214
Wei Ning <macgnw@gmail.com>
2019-12-21 19:28:20 +1100

    blockchain

---
+++ blockchain/README.md
@@ -0,0 +1,3 @@
+# Learn Blockchains by Building One
+
+source: <https://hackernoon.com/learn-blockchains-by-building-one-117428612f46>
--- blockchain/src/blockchain/app.py
+++ blockchain/src/blockchain/app.py
@@ -1,4 +1,3 @@
-import json
 import uuid
 
 import flask
@@ -6,24 +5,56 @@ import flask
 from .lib import BlockChain
 
 app = flask.Flask('simple blockchain')
-node_identifier = uuid.uuid4()
+node_identifier = str(uuid.uuid4()).replace('-', '')
 blockchain = BlockChain()
 
 
 @app.route('/mine', methods=['GET'])
 def mine():
-    return 'mining a new block'
+    """
+    TO do three things:
+
+    - calculate the proof of work
+    - reward the miner by adding a transaction granting us 1 coin
+    - forge the new Block by adding it to the chain
+    """
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    blockchain.new_transaction(sender='0', recipient=node_identifier, amount=1)
+
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    resp = {
+        'message': 'New Block has been forged',
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash']
+    }
+    return flask.jsonify(resp), 200
 
 
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
-    return 'adding a new transaction'
+    values = flask.request.get_json()
+    if not (values
+            and all([values.get(k)
+                     for k in ('sender', 'recipient', 'amount')])):
+        return 'Missing values', 400
+
+    index = blockchain.new_transaction(values['sender'], values['recipient'],
+                                       values['amount'])
+    resp = {'message': f'Transaction will be added to Block {index}'}
+    return flask.jsonify(resp), 201
 
 
 @app.route('/chain', methods=['GET'])
 def full_chain():
     response = {'chain': blockchain.chain, 'length': len(blockchain.chain)}
-    return json.jsonify(response), 200
+    return flask.jsonify(response), 200
 
 
 def main():
