89c1839cb83f0ee29a4682f7ba667f9fc508b42b
Wei Ning (DA) <wei.ning@digitalasset.com>
2021-07-15 17:49:18 +1000

    explore proof of work

---
+++ bcscratch/tests/test_proof_of_work.py
@@ -0,0 +1,30 @@
+import hashlib
+import random
+import time
+import unittest
+
+
+def generate(size: int) -> [int]:
+    return random.choices(list(range(100)), k=size)
+
+
+def do_hash(payload: [int]) -> str:
+    return hashlib.sha256(bytes(payload)).hexdigest()
+
+
+def prove(h: str) -> bool:
+    return int('0x' + h[-7:], 16) < 1000
+
+
+def trial() -> [int]:
+    s = time.time()
+    while True:
+        payload = generate(12)
+        if prove(do_hash(payload)):
+            return payload, time.time() - s
+
+
+class TestProofOfWork(unittest.TestCase):
+    def test_average_time(self):
+        _, time_taken = trial()
+        self.assertGreater(time_taken, 0.0001)
