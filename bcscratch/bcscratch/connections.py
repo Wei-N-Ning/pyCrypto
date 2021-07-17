26a189a8692c03aec5aaade37b6a946f73cdc4db
Wei Ning (DA) <wei.ning@digitalasset.com>
2021-07-17 14:24:44 +1000

    wip

--- bcscratch/bcscratch/connections.py
+++ bcscratch/bcscratch/connections.py
@@ -4,15 +4,28 @@ active connections communicating with our node
 
 """
 
-from asyncio import StreamWriter
+from bcscratch.types import *
+
+from typing import *
+from more_itertools import take
 
 
 class ConnectionPool:
     def __init__(self):
-        self.pool = set()
+        self.pool: Dict[str, Peer] = dict()
 
-    def add_peer(self, w: StreamWriter):
-        self.pool.add(w)
+    def add_peer(self, p: Peer):
+        self.pool[p.as_str()] = p
 
-    def remove_peer(self, w: StreamWriter):
-        self.pool.remove(w)
+    def remove_peer(self, p: Peer):
+        try:
+            del self.pool[p.as_str()]
+        except KeyError:
+            pass
+
+    def get_alive_peers(self, count: int) -> List[Peer]:
+        return take(count, self.pool.values())
+
+    def broadcast(self, msg: str):
+        for _, p in self.pool.items():
+            p.write(msg)
